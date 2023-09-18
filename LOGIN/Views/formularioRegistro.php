4d3df6d889afd234c3b64a7b2282a2535d98672a
PatiLeo87 <114914710+PatiLeo87@users.noreply.github.com>
2023-09-18 11:41:46 +0200

    VistaBootstrap

--- LOGIN/Views/formularioRegistro.php
+++ LOGIN/Views/formularioRegistro.php
@@ -3,6 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuario</title>
+    <!-- Incluye los estilos de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="text-center">Registro de Usuario</h1>
+                <form action="./index.php" method="POST">
+                    <input type="hidden" name="registrar" id="registrar" value="1">
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Usuario:</label>
+                        <input type="text" id="usuario" name="usuario" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre:</label>
+                        <input type="text" id="nombre" name="nombre" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="apellido" class="form-label">Apellido:</label>
+                        <input type="text" id="apellido" name="apellido" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Contraseña:</label>
+                        <input type="password" id="password" name="password" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" id="email" name="email" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Registrarse</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Incluye los scripts de Bootstrap al final del documento (antes de </body>) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <style>
         h1{
@@ -46,3 +93,4 @@
     </form>
 </body>
 </html>
+ -->
--- LOGIN/Views/login.php
+++ LOGIN/Views/login.php
@@ -4,12 +4,54 @@
 require_once(__DIR__ . '/../Configs/params.php');
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Inicio de Sesión</title>
+    <!-- Incluye los estilos de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="./index.php" method="post">
+                    <h2 class="text-center">Inicio de Sesión</h2>
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Usuario:</label>
+                        <input type="text" id="usuario" name="usuario" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="contrasena" class="form-label">Contraseña:</label>
+                        <input type="password" id="contrasena" name="contrasena" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+                </form>
+                <h2 class="mt-4 text-center">¿No tienes cuenta? Regístrate.</h2>
+                <form action="./index.php" method="post">
+                    <input type="hidden" id="registrar" name="registrar" value="1">
+                    <button type="submit" class="btn btn-success btn-block">Regístrate</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Incluye los scripts de Bootstrap al final del documento (antes de </body>) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
     <style>
         form{
             width: 300px;
@@ -30,22 +72,6 @@ require_once(__DIR__ . '/../Configs/params.php');
     </style>
 </head>
 
-<?php
-/* if(isset($_POST['usuario']) && $_POST['usuario'] !=''){
-
-    $usuario = new Usuario();
-    $existe = $usuario->validarInicioSesion($_POST['usuario'], $_POST['contrasena']);
-
-    if($existe){
-        echo "entramos!";
-        header('location:principal.php');
-    }else{
-        echo "Usuario o contraseña errónea.";
-    }
-
-} */
-
-?>
 <body>
     <form action="./index.php" method="post">
         <label for="">Usuario:</label>
@@ -65,4 +91,4 @@ require_once(__DIR__ . '/../Configs/params.php');
    
 
 </body>
-</html>
+</html> -->
